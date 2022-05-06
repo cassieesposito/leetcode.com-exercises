749846ed4c51ccf664edae35244987ec97f857d9
Cassie Esposito <dev@timevault.org>
2022-05-05 18:54:44 -0700

    ("sippi", "s*is*p*.") handled correctly

--- 10. Regular Expression Matching/index.js
+++ 10. Regular Expression Matching/index.js
@@ -7,20 +7,24 @@
 ANY = '.'
 REPEAT = '*'
 
-isMatch = (string,pattern) => {
-    s=[...string]
+isMatch = (string, pattern) => {
+    s = [...string]
     p = [...pattern]
     sI = pI = 0
 
     while (sI < s.length || pI < p.length) {
-        if (p[pI] == s[sI] || p[pI] == ANY) { sI++; pI++; continue }
-        if (p[pI+1] == REPEAT) { pI++; continue }
+        if (p[pI] == s[sI] || p[pI] == ANY) {
+            sI++;
+            pI++;
+            continue
+        }
+        if (p[pI + 1] == REPEAT) { pI++; continue }
 
         // Recursive handler
-        if (p[pI] == REPEAT) { debugger; return handleStar(string.slice(sI-1), pattern.slice(pI+1), p[pI-1]) }
+        if (p[pI] == REPEAT) { debugger; return handleStar(string.slice(sI == 0 ? 0 : sI - 1), pattern.slice(pI + 1), p[pI - 1]) }
 
-        while (s[sI+1] >= s.length && p[pI+1] < p.length) {
-            if (p[pI+2] == REPEAT) {pI += 2; continue}
+        while (s[sI + 1] >= s.length && p[pI + 1] < p.length) {
+            if (p[pI + 2] == REPEAT) { pI += 2; continue }
             return false
         }
         return false
@@ -28,14 +32,19 @@ isMatch = (string,pattern) => {
     return true
 }
 
-handleStar = (string, pattern, character) => { 
-    if (!pattern) {return true}
+handleStar = (string, pattern, character) => {
+    if (!pattern) { return true }
     while (string) {
         debugger
-        if (isMatch(string, pattern)) {debugger; return true}
-        if (character == ANY || character == string.charAt(0)) { string = string.slice(1); continue }
+        if (isMatch(string, pattern)) { debugger; return true }
+        if (character == ANY || character == string.charAt(0)) {
+            debugger;
+            string = string.slice(1);
+            debugger
+            continue
+        }
         return false
     }
 }
 
-console.log(isMatch("mississippi","mis*is*p*."))
+console.log(isMatch("sippi", "s*is*p*."))
