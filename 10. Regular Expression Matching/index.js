55dcba18c0e885b479504342bd4a397f63bc4905
Cassie Esposito <dev@timevault.org>
2022-05-10 13:27:00 -0700

    Still not working, need to fix isMatch("aaa", "ab*ac*a")

--- 10. Regular Expression Matching/index.js
+++ 10. Regular Expression Matching/index.js
@@ -18,13 +18,21 @@ isMatch = (string, pattern) => {
             pI++;
             continue
         }
-        if (p[pI + 1] == REPEAT) { pI++; continue }
+        if (p[pI + 1] == REPEAT) {
+            pI++
+            continue
+        }
 
         // Recursive handler
-        if (p[pI] == REPEAT) { debugger; return handleStar(string.slice(sI == 0 ? 0 : sI - 1), pattern.slice(pI + 1), p[pI - 1]) }
+        if (p[pI] == REPEAT) {
+            return handleStar(string.slice(sI == 0 ? 0 : sI - 1), pattern.slice(pI - 1))
+        }
 
         while (s[sI + 1] >= s.length && p[pI + 1] < p.length) {
-            if (p[pI + 2] == REPEAT) { pI += 2; continue }
+            if (p[pI + 2] == REPEAT) {
+                pI += 2
+                continue
+            }
             return false
         }
         return false
@@ -32,19 +40,22 @@ isMatch = (string, pattern) => {
     return true
 }
 
-handleStar = (string, pattern, character) => {
-    if (!pattern) { return true }
-    while (string) {
+handleStar = (string, pattern) => {
+    while (true) {
         debugger
-        if (isMatch(string, pattern)) { debugger; return true }
-        if (character == ANY || character == string.charAt(0)) {
-            debugger;
-            string = string.slice(1);
-            debugger
+        if (isMatch(string, pattern.slice(2))) {
+            return true
+        }
+        if (!string && pattern[0] == ANY) {
+            pattern = pattern.slice(2)
+        }
+        if (pattern[0] == ANY || pattern[0] == string[0]) {
+            string = string.slice(1)
             continue
         }
+        debugger
         return false
     }
 }
 
-console.log(isMatch("sippi", "s*is*p*."))
+console.log(isMatch("aaa", "ab*ac*a"))
