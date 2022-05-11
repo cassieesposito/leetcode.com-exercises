53abcbbe21dea1cc527a9c30352751418d07f63f
Cassie Esposito <dev@timevault.org>
2022-05-10 18:42:46 -0700

    Refactored to increase computational efficiency since validated input is assumed.

--- 13. Roman to Integer/main.py
+++ 13. Roman to Integer/main.py
@@ -3,18 +3,15 @@ CONST_ROMAN_VALUES = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "
 
 class Solution:
     def romanToInt(self, roman: str) -> int:
-        arabic = 0
 
-        for i, numeral in enumerate(roman):
-            if str(CONST_ROMAN_VALUES[numeral])[0] == "1" and len(roman) > i + 1:
-                if CONST_ROMAN_VALUES[numeral] in (
-                    CONST_ROMAN_VALUES[roman[i + 1]] / 5,
-                    CONST_ROMAN_VALUES[roman[i + 1]] / 10,
-                ):
-                    arabic -= 2 * CONST_ROMAN_VALUES[numeral]
-            arabic += CONST_ROMAN_VALUES[numeral]
+        lastIndex = len(roman) - 1
+        arabic = CONST_ROMAN_VALUES[roman[lastIndex]]
+        for i in range(lastIndex):
+            if CONST_ROMAN_VALUES[roman[i]] < CONST_ROMAN_VALUES[roman[i + 1]]:
+                arabic -= 2 * CONST_ROMAN_VALUES[roman[i]]
+            arabic += CONST_ROMAN_VALUES[roman[i]]
 
         return arabic
 
 
-print(Solution().romanToInt("MMCDI"))
+print(Solution().romanToInt("VXXVI"))
